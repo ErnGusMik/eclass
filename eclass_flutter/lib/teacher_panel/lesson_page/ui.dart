132f251e3a7c753a7d68ed8e0945bc08e8cb08d4
ErnGusMik <77016061+ErnGusMik@users.noreply.github.com>
2025-06-15 22:13:56 +0300

    Started lesson card UI

--- eclass_flutter/lib/teacher_panel/lesson_page/ui.dart
+++ eclass_flutter/lib/teacher_panel/lesson_page/ui.dart
@@ -61,45 +61,106 @@ class _LessonsSectionState extends State<LessonsSection> {
             ),
           ),
         ),
-        Container(
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surfaceContainerLowest,
-            borderRadius: BorderRadius.circular(16.0)
-          ),
-          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
-          child: Column(
-            spacing: 10.0,
+        LessonCard(),
+      ],
+    );
+  }
+}
+
+class LessonCard extends StatefulWidget {
+  const LessonCard({super.key});
+
+  @override
+  State<LessonCard> createState() => _LessonCardState();
+}
+
+class _LessonCardState extends State<LessonCard> {
+  TextEditingController notesController = TextEditingController();
+  FocusNode notesFocusNode = FocusNode();
+  bool editingNotes = false;
+
+  @override
+  void initState() {
+    super.initState();
+    notesController.text =
+    "Hi class!\nNew topic today -- be ready! I will quiz you on your current knowledge. Don't worry, this will not impact your grades.";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceContainerLowest,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+      child: Column(
+        spacing: 10.0,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("08:20 - 09:00", style: Theme.of(context).textTheme.titleMedium),
-                  Text("Room 302", style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant))
-                ],
+              Text(
+                "08:20 - 09:00",
+                style: Theme.of(context).textTheme.titleMedium,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 4.0,
-                children: [
-                  Text("Notes to class", style: Theme.of(context).textTheme.labelSmall),
-                  Row(
-                    spacing: 4.0,
-                    children: [
-                      Expanded(
-                        child: Text("Hi class!\nNew topic today -- be ready! I will quiz you on your current knowledge. Don't worry, this will not impact your grades.", style: Theme.of(context).textTheme.bodySmall),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.edit_outlined),
-                        onPressed: () {},
-                      )
-                    ],
-                  )
-                ],
-              )
+              Text(
+                "Room 302",
+                style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+              ),
             ],
           ),
-        )
-      ],
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 4.0,
+            children: [
+              Text(
+                "Notes to class",
+                style: Theme.of(context).textTheme.labelSmall,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: notesController,
+                      focusNode: notesFocusNode,
+                      enabled: editingNotes,
+                      decoration: InputDecoration.collapsed(
+                        hintText: 'None',
+                        hintStyle: Theme.of(
+                          context,
+                        ).textTheme.bodyMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                      maxLines: null,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      editingNotes ? Icons.check : Icons.edit_outlined,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        editingNotes = !editingNotes;
+                      });
+                      if (editingNotes) {
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          notesFocusNode.requestFocus();
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
